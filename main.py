8905f849659eee22e027d4a3f997be994202529c
zoeaika <95620159+zoeaika@users.noreply.github.com>
2025-04-18 23:45:11 +0300

    Update and rename week1.py to main.py

--- main.py
+++ main.py
@@ -49,20 +49,3 @@ class parrot(pet1):
             print(f"{self.name} is flying high in the sky!")
         else:
             print(f"{self.name} is too tired to fly.")
-    
-pet1 = pet1("Kifaru")
-pet1.get_status()
-pet1.eat()
-pet1.sleep()
-pet1.play()
-pet1.get_status()
-pet1.train("roll over")
-pet1.train("play dead")
-pet1.show_tricks()
-
-#pet 2
-pet2 = parrot("Laiti")
-pet2.eat()
-pet2.sleep()
-pet2.fly()
-pet2.get_status()
