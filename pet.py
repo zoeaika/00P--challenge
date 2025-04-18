a82267b2ff7898c844beb1ea3c9e1d7ff7833cb4
zoeaika <95620159+zoeaika@users.noreply.github.com>
2025-04-18 23:46:24 +0300

    Create pet.py

---
+++ pet.py
@@ -0,0 +1,17 @@
+#pet1
+pet1 = pet1("Kifaru")
+pet1.get_status()
+pet1.eat()
+pet1.sleep()
+pet1.play()
+pet1.get_status()
+pet1.train("roll over")
+pet1.train("play dead")
+pet1.show_tricks()
+
+#pet 2
+pet2 = parrot("Laiti")
+pet2.eat()
+pet2.sleep()
+pet2.fly()
+pet2.get_status()
